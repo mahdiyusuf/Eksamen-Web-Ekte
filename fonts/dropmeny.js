0d4152f9821a5933902402ac99cf06dc5f57dd11
s364517 <89968022+s364517@users.noreply.github.com>
2021-11-25 23:09:08 +0100

    for now

--- fonts/dropmeny.js
+++ fonts/dropmeny.js
@@ -2,7 +2,7 @@ console.log("Dette er ikke kompatibelt")
 
 document.getElementById("menu").addEventListener("click", openMenu);
 
-function openMenu{
+function openMenu() {
 
     document.getElementById("dropdown").classList.toggle("active");
 }
