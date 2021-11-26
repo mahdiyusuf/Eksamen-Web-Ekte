9c76e84b1dac74aa5213a612561daea0dcfe1c68
s364517 <89968022+s364517@users.noreply.github.com>
2021-11-26 15:46:07 +0100

    Nesten ferdug

--- fonts/dropmeny.js
+++
@@ -1,8 +0,0 @@
-console.log("Dette er ikke kompatibelt")
-
-document.getElementById("menu").addEventListener("click", openMenu);
-
-function openMenu() {
-
-    document.getElementById("dropdown").classList.toggle("active");
-}
